3012903aee4ec100648e66a9d75a9fd52e4e634c
Mariano-Pinto <mampcpel@gmail.com>
2025-10-26 20:48:58 -0300

    Subo los archivos app.py del Front End y del Back end Co-Authors-by:mmolloja@fi.uba.ar Mariano

---
+++ APIRESTFUL/api/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
+# Agregué jsonify lo demás son funciones del último tp
+
+
+
+app = Flask(__name__)
+
+@app.route('/users', methods = ['GET'])
+def users ():
+    
+    result = {"id": 1, "name": "Mariano", "email": "mampcpel@gmail.com", "active": True}
+        
+    data=[]
+    for row in result:
+              entity['id'] = row['id']
+              entity['name'] = row['name']
+              entity['email'] = row['email']
+              entity['active'] = row['active']
+              data.append(entity)
+
+    return jsonify(data),200
+
+@app.route('/user/<id>', methods = ['GET'])
+def users (id):
+    result = {"id": 1, "name": "Mariano", "email": "mampcpel@gmail.com", "active": True}
+    for row in result:
+         if row('id') == int(id):
+              entity['id'] = row['id']
+              entity['name'] = row['name']
+              entity['email'] = row['email']
+              entity['active'] = row['active']
+              data.append(entity)
+
+    return jsonify(data),200
+
+
+
+if __name__== '__main__':
+        app.run("127.0.0.1", port="5000", debug=True)
