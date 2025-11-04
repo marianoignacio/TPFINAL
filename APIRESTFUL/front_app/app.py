757eff38f588de78440ba6860a3f39ae50da6240
Dylan <druiz@fi.uba.ar>
2025-11-04 02:43:55 -0300

    Añado la logica al app.py del front para que renderice la pagina index, ademas, linkeo con url for las imagenes, el js, y css( aun falta ver si esta todo el css en all.css) falta que se muestre el texto en la pagina y luego modificarla para que encaje con el mockup

--- APIRESTFUL/front_app/app.py
+++ APIRESTFUL/front_app/app.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
 
 import json
-import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home ():
+    return render_template('index.html')
+
+if __name__== '__main__':
+        app.run("localhost", port=8088, debug=True)
