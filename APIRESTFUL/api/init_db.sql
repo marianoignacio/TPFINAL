0d15290f321899f83cb3b3008d67f92c5355fa65
Dylan <druiz@fi.uba.ar>
2025-11-16 20:03:09 -0300

    Agrego las tablas usuario y habitaciones, ademas de modificarl la tabal reservas a la hora de crearlas en la DB con el init.sql para que encajen con el sistema pensado

--- APIRESTFUL/api/init_db.sql
+++ APIRESTFUL/api/init_db.sql
@@ -1,9 +1,33 @@
 CREATE DATABASE IF NOT EXISTS hotel;
 USE hotel;
+CREATE TABLE IF NOT EXISTS usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255),
+    apellido VARCHAR(255),
+    contraseña VARCHAR(255),
+    email VARCHAR(255),
+    fecha_creacion DATE
+);
+
+CREATE TABLE IF NOT EXISTS habitaciones (
+    id_habitacion INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255),
+    capacidad INT,
+    descripcion VARCHAR(255),
+    servicios VARCHAR(255),
+    precio_noche FLOAT
+);
+
 CREATE TABLE IF NOT EXISTS reservas (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+    id_habitacion INT,
     check_in DATE,
-    check_out DATE
+    check_out DATE,
+    huespedes TINYINT,
+    monto_total FLOAT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
+    FOREIGN KEY (id_habitacion) REFERENCES habitaciones(id_habitacion) ON DELETE CASCADE
 );
 
 INSERT INTO reservas (check_in, check_out) VALUES
