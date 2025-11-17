f0627b8324207de1b3a15d676315cc7e917d681b
marianoignacio <mmolloja@fi.uba.ar>
2025-11-17 05:14:19 -0300

    Agrego endpoints de reserva

--- APIRESTFUL/api/app.py
+++ APIRESTFUL/api/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.usuarios import usuarios_bp
+from routes.reservas import reservas_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(usuarios_bp, url_prefix="/usuarios")
+app.register_blueprint(reservas_bp, url_prefix="/reservas")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
