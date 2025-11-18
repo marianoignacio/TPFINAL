669667541c7cd8202d7125591ba625fdcb6a2851
abecediaz <abecediaz@gmail.com>
2025-11-18 15:31:34 -0300

    Desarrollo de los últimos tres métodos para usuarios definidos de la API.

--- APIRESTFUL/api/routes/usuarios.py
+++ APIRESTFUL/api/routes/usuarios.py
@@ -79,3 +79,73 @@ def crear_usuario(id_usuario):
     cursor.close()
     conn.close()
     return ("Usuario creado correctamente", 201)
+
+#PUT --> /usuarios/<id_usuario> --> Actualiza un usuario con id_usuario indicado.
+@usuarios_bp.route("/<int:id_usuario>", methods=["PUT"])
+def actualizar_usuario(id_usuario):
+    conn = conectarse_db()
+    cursor = conn.cursor(dictionary=True)
+    data = request.json
+
+    nombre = data.get("nombre")
+    apellido = data.get("apellido")
+    contrasenia = data.get("contraseña")
+    email = data.get("email")
+    fecha_creacion = data.get("fecha_creacion")
+
+    cursor.execute("SELECT id_usuario FROM usuarios WHERE id_usuario = %s", (id_usuario,))
+    if cursor.fetchone() is None:
+        cursor.close()
+        conn.close()
+        return ("Usuario no encontrado", 404)
+
+    cursor.execute("""
+                   UPDATE usuarios
+                   SET nombre=%s, apellido=%s, contraseña=%s, email=%s, fecha_creacion=%s
+                   WHERE id_usuario=%s
+                   """, (nombre, apellido, contrasenia, email, fecha_creacion, id_usuario))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return ("Usuario actualizado correctamente", 200)
+
+#DELETE --> /usuarios/<id_usuario> --> Elimina un usuario con el <id_usuario> indicado.
+@usuarios_bp.route("/<int:id_usuario>", methods=["DELETE"])
+def eliminar_usuario(id_usuario):
+    conn = conectarse_db()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT id_usuario FROM usuarios WHERE id_usuario = %s", (id_usuario,))
+    if cursor.fetchone() is None:
+        cursor.close()
+        conn.close()
+        return ("Usuario no encontrado", 404)
+
+    cursor.execute("DELETE FROM usuarios WHERE id_usuario = %s", (id_usuario,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return ("Usuario eliminado correctamente", 200)
+
+#DELETE --> /usuarios/<id_usuario>/reservas --> Elimina las reservas del usuario con el <id_usuario> indicado.
+@usuarios_bp.route("/<int:id_usuario>/reservas", methods=["DELETE"])
+def eliminar_reservas_usuario(id_usuario):
+    conn = conectarse_db()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT id FROM reservas WHERE id_usuario = %s", (id_usuario,))
+    reservas = cursor.fetchall()
+
+    if not reservas:
+        cursor.close()
+        conn.close()
+        return ("El usuario no tiene reservas", 404)
+
+    cursor.execute("DELETE FROM reservas WHERE id_usuario = %s", (id_usuario,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return ("Reservas del usuario eliminadas correctamente", 200)
